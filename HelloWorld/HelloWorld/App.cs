64aabc5c35feedb156ccac9a2d7648ef45092f6a
sunhara <Tom_gaoshen@163.com>
2023-09-28 14:18:51 +0800

    update

--- HelloWorld/HelloWorld/App.cs
+++ HelloWorld/HelloWorld/App.cs
@@ -10,6 +10,10 @@ using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using Autodesk.Revit.DB.Architecture;
 using System.Reflection;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Drawing.Imaging;
+
 
 namespace ribbon_and_tab
 {
@@ -20,18 +24,32 @@ namespace ribbon_and_tab
             string tabname = "GuoZhu_Solution";
             string panelname = "tutorial";
 
+            //Bitimages
+            BitmapImage bt1image = new BitmapImage(new Uri("C:\\Users\\6321011\\Desktop\\icons8.png"));
+
             //Creating the tab
             application.CreateRibbonTab(tabname);
             //Creating the panel
             var panel = application.CreateRibbonPanel(tabname, panelname);
             //Creating the buttons
-            var button1 = new PushButtonData("button1_internalName", "User_visibleName", Assembly.GetExecutingAssembly().Location, "HelloWorld.ShowDialog");
+            var button1 = new PushButtonData("button1_internalName", "User_visibleName1", Assembly.GetExecutingAssembly().Location, "HelloWorld.ShowDialog");
 
             button1.ToolTip = "Here for tooltip";
             button1.LongDescription = "This is a long version description";
+            button1.LargeImage = bt1image;
 
-            var btn1 = panel.AddItem(button1) as PushButton;
+            panel.AddItem(button1);
 
+            //The stacked buttons
+            var button2 = new PushButtonData("button2_internalName", "User_visibleName2", Assembly.GetExecutingAssembly().Location, "HelloWorld.ShowDialog");
+            button2.ToolTip = "Here for tooltip2";
+            button2.LongDescription = "This is a long version description2";
+
+            var button3 = new PushButtonData("button3_internalName", "User_visibleName3", Assembly.GetExecutingAssembly().Location, "HelloWorld.ShowDialog");
+            button3.ToolTip = "Here for tooltip3";
+            button3.LongDescription = "This is a long version description3";
+
+            panel.AddStackedItems(button2, button3);
 
             return Result.Succeeded;
         }
